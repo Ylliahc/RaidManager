b349b04a4e3b92590e088b84c3797c0607ac3ca5
Xavier Chailly <xavier.chailly.prest@sogetrel.fr>
2021-05-19 17:17:49 +0200

    prepare Auth

--- src/RaidManager.Domain/Dto/Authentication/LoginResponse.cs
+++ src/RaidManager.Domain/Dto/Authentication/LoginResponse.cs
@@ -1,5 +1,8 @@
 namespace RaidManager.Domain.Dto.Authentication
 {
+    /// <summary>
+    /// Model for login response
+    /// </summary>
     public class LoginResponse
     {
         
---
+++ src/RaidManager.Domain/Dto/DtoBase.cs
@@ -0,0 +1,7 @@
+namespace RaidManager.Domain.Dto
+{
+    public class DtoBase
+    {
+        public string Id { get; set; }
+    }
+}
