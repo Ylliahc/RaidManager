dc7d770192978b5abe37a3e65e1ed537ecc5826e
Ylliahc <ylliahc@hotmail.fr>
2021-08-06 14:02:11 +0200

    Add configuration in appsettings file

--- src/RaidManager.Infrastructure/Configurations/DataBaseConfiguration.cs
+++ src/RaidManager.Infrastructure/Configurations/DataBaseConfiguration.cs
@@ -1,7 +1,19 @@
 namespace RaidManager.Infrastructure.Configurations
 {
+    /// <summary>
+    /// Model for database configuration
+    /// </summary>
     public class DataBaseConfiguration
     {
+        /// <summary>
+        /// Connection string
+        /// </summary>
+        /// <value></value>
         public string ConnectionString {get;set;}
+        /// <summary>
+        /// Queries configuration
+        /// </summary>
+        /// <value></value>
+        public QueriesConfiguration QueriesConfiguration {get;set;}
     }
 }
